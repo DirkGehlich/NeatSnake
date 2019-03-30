78ef779e786eb66866a0e748bba3e49c214ae8cd
Kreatief <mucus2@gmail.com>
2019-03-30 20:26:11 +0100

    changed port for rest server to 8080

--- src/main/java/server/GameServer.java
+++ src/main/java/server/GameServer.java
@@ -10,7 +10,7 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 public class GameServer {
 	public static void main( String[] args ) throws IOException, InterruptedException 
 	   {
-	      String baseUrl = ( args.length > 0 ) ? args[0] : "http://localhost:4434";
+	      String baseUrl = ( args.length > 0 ) ? args[0] : "http://localhost:8080";
 
 	      final HttpServer server = GrizzlyHttpServerFactory.createHttpServer(
 	            URI.create( baseUrl ), new ResourceConfig( GameService.class ), false );
